3a9a4e72d292edcd8e5aaabfc3d809a986671e43
kickinbahk <kickinbahk@gmail.com>
2016-02-13 14:59:37 -0800

    Takes city as an optional argument and will use it to get location. Otherwise, will attempt to get user location by IP address.

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 var weather = require("./weather.js");
 var location = require("./location.js");
+var argv = require("yargs")
+        .option("location", {
+            alias: 'l',
+            demand: false,
+            describe: "Location to fetch weather",
+            type: "string"
+        }).help("help")
+        .argv;
 
-weather(function (currentWeather) {
-    console.log(currentWeather);
-});
-
-location(function (location) {
-    if (!location) {
-        console.log(`Not able to get location`);
-        return;
-    }
-    console.log(`city: ${location.city}`);
-    console.log(`long/lat: ${location.loc}`);    
-});
-
+if (typeof argv.l === "string" && argv.l.length > 0) {
+    console.log(`The city ${argv.l} was provided.`);
+    weather(argv.l, function(currentWeather) {
+        console.log(currentWeather);
+    });
+} else {
+    console.log(`No city was given. Attempting to use IP address to get location.`);
+    location(function (location) {
+        if (location) {
+            weather(location.city, function(currentWeather) {
+                console.log(currentWeather);
+            });
+        } else {
+            console.log("Not able to get location");
+        }
+    });
+}
--- weather.js
+++ weather.js
@@ -1,13 +1,17 @@
 var request = require("request");
-var url = "http://api.openweathermap.org/data/2.5/weather?q=LosAngeles&units=imperial&appid=44db6a862fba0b067b1930da0d769e98";
 
-module.exports = function (callback) {
+module.exports = function (location, callback) {
+    var encodedLocation = encodeURIComponent(location);
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${encodedLocation}&units=imperial&appid=44db6a862fba0b067b1930da0d769e98`;
 
+    if (!location) {
+        callback(`No location provided`);
+    }
+    
     request({url: url, json: true}, function(err, response, body) {
         if (err) {
             callback();
         } else {
-            // console.log(JSON.stringify(body, null, 4));
             callback(`It is ${body.main.temp} degrees in ${body.name}`);
         }
     });
