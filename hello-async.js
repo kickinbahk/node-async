34698f15d2c8896438687d6a41500fd9a32f995c
kickinbahk <kickinbahk@gmail.com>
2016-02-13 13:18:52 -0800

    add function to print message after 2 sec delay

--- hello-async.js
+++ hello-async.js
@@ -11,14 +11,24 @@
 // console.log(2);
 
 // **** Showing further Async functionality  ****
-setTimeout(function() {
-    console.log(1);
-}, 2000);
+// setTimeout(function() {
+//     console.log(1);
+// }, 2000);
 
-setTimeout(function() {
-    console.log(3);
-}, 1000); 
+// setTimeout(function() {
+//     console.log(3);
+// }, 1000); 
 
-console.log(2);
+// console.log(2);
+
+function printInTwoSeconds (message) {
+    setTimeout(function () {
+        console.log(message);
+    }, 2000);
+}
+
+printInTwoSeconds("Hello, Async");
+
+
 
 
