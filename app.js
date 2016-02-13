4ef907709606e82469a2245bd73f62a35ebb356c
kickinbahk <kickinbahk@gmail.com>
2016-02-13 14:05:48 -0800

    move functionality to module

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-var request = require("request");
-var url = "http://api.openweathermap.org/data/2.5/weather?q=LosAngeles&units=imperial&appid=44db6a862fba0b067b1930da0d769e98";
+var weather = require("./weather.js");
 
-request({url: url, json: true,}, function(err, response, body) {
-    if (err) {
-        console.log("Unable to fetch weather");
-    } else {
-        console.log(JSON.stringify(body, null, 4));
-        console.log(`It is ${body.main.temp} degrees in ${body.name}`);
-    }
+weather(function (currentWeather) {
+    console.log(currentWeather);
 });
 
--- weather.js
+++ weather.js
@@ -1,5 +1,16 @@
-module.exports = function () {
-    console.log(`Got weather`);
-}
+var request = require("request");
+var url = "http://api.openweathermap.org/data/2.5/weather?q=LosAngeles&units=imperial&appid=44db6a862fba0b067b1930da0d769e98";
+
+module.exports = function (callback) {
+
+    request({url: url, json: true}, function(err, response, body) {
+        if (err) {
+            callback("Unable to fetch weather");
+        } else {
+            // console.log(JSON.stringify(body, null, 4));
+            callback(`It is ${body.main.temp} degrees in ${body.name}`);
+        }
+    });
+};
 
 
