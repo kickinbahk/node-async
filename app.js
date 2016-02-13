58cc3b5003b7b8f67a9deb0e92eecb4a3ab8cb44
kickinbahk <kickinbahk@gmail.com>
2016-02-13 13:48:45 -0800

    move functionality to app.js and add module weather.js

---
+++ app.js
@@ -0,0 +1,12 @@
+var request = require("request");
+var url = "http://api.openweathermap.org/data/2.5/weather?q=LosAngeles&units=imperial&appid=44db6a862fba0b067b1930da0d769e98";
+
+request({url: url, json: true,}, function(err, response, body) {
+    if (err) {
+        console.log("Unable to fetch weather");
+    } else {
+        console.log(JSON.stringify(body, null, 4));
+        console.log(`It is ${body.main.temp} degrees in ${body.name}`);
+    }
+});
+
