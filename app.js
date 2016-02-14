5551e4e27d4eab1063e61c270054a611e7a2c8ad
kickinbahk <kickinbahk@gmail.com>
2016-02-14 10:58:56 -0800

    Remove commented old code

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ var argv = require("yargs")
 
 if (typeof argv.l === "string" && argv.l.length > 0) {
     console.log(`The city ${argv.l} was provided.`);
-    // weather(argv.l, function(currentWeather) {
-    //     console.log(currentWeather);
-    // });
     weather(argv.l).then(function (currentWeather) {
         console.log(currentWeather);
     }).catch(function () {
@@ -21,15 +18,6 @@ if (typeof argv.l === "string" && argv.l.length > 0) {
     });
 } else {
     console.log(`No city was given. Attempting to use IP address to get location.`);
-    // location(function (location) {
-    //     if (location) {
-    //         weather(location.city, function(currentWeather) {
-    //             console.log(currentWeather);
-    //         });
-    //     } else {
-    //         console.log("Not able to get location");
-    //     }
-    // });
     location().then(function (locObj) {
         return weather(locObj.city);
     }).then(function (currentWeather) {
